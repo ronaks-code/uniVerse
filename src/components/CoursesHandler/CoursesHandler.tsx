b061c0f4979b7a80d14ebaeeb2081b562f2ea105
Ronak Agarwal <agarwalronak58@gmail.com>
2023-07-09 02:15:51 -0400

    In-progress

---
+++ src/components/CoursesHandler/CoursesHandler.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+class CoursesHandler extends React.Component {
+  // Logic for adding/removing courses goes here
+
+  render() {
+    return (
+      <div className="courses-handler">
+        {/* Add/Remove Courses UI goes here */}
+      </div>
+    );
+  }
+}
+
+export default CoursesHandler;
